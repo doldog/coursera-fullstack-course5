7b970245d4acd32e7e6b2e533880a65d03980120
doldog <davekoukai@gmail.com>
2018-11-10 11:49:20 -0800

    Assignment 1

---
+++ module1_sol/app.js
@@ -0,0 +1,27 @@
+const thresholdForTooMuch = 3;
+
+(function(){
+    'use strict';
+    angular.module('LunchCheck',[]).controller('LunchCheckController', LunchCheckController);
+    LunchCheckController.$inject = ['$scope'];
+    function LunchCheckController($scope){
+        $scope.message = "";
+        $scope.inputTextBox = "";
+        $scope.check = ()=>{
+            let input = $scope.inputTextBox;
+            let inputArray = input.split(",");
+            let count = 0;
+            let message = "Enjoy";
+            for(let i = 0; i<inputArray.length; i++){
+                if (inputArray[i].trim() !== ""){
+                    count ++;
+                }
+            }
+            count>thresholdForTooMuch ? message = "Too much":"Enjoy";
+            $scope.message = message;
+        }
+
+    }
+
+
+})();
